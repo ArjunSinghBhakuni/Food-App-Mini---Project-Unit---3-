c56b5cc716b52995e324e8e594309227500f6719
Arjun Singh <101566727+ArjunSinghBhakuni@users.noreply.github.com>
2022-05-02 17:31:23 +0530

    Add files via upload

---
+++ food app/components/navbar.js
@@ -0,0 +1,27 @@
+
+  function navbar(){
+
+ return `
+<div id="nav1">
+        <p>
+          <a href="index.html">HOME</a>
+        </p>
+        <p>
+          <a href="receipe.html">Categories</a>
+        </p>
+        <p>
+          <a href="random.html">Random Receipe</a>
+        </p>
+      </div>
+      <div id="nav2">
+        <p>
+         <a href="login.html">Log In</a>
+        </p>
+        <p>
+        <a href="signup.html">Sign Up</a>
+        </p>
+      </div>`;
+
+ }
+
+  export default navbar;
